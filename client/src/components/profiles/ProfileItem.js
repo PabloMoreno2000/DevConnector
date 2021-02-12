d22017c1361cdd41b71b5e921b34c6058c516b12
PabloMoreno2000 <pamta2000@gmail.com>
2021-02-12 16:22:40 -0600

    Developers are shown

---
+++ client/src/components/profiles/ProfileItem.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+// From profile get user, from user get id, name, avatar
+const ProfileItem = ({
+  profile: {
+    user: { _id, name, avatar },
+    status,
+    company,
+    location,
+    skills,
+  },
+}) => {
+  return (
+    <div className="profile bg-light">
+      <img src={avatar} alt=" " className="round-img" />
+      <div>
+        <h2>{name}</h2>
+
+        <p>
+          {status}{" "}
+          {
+            // If there is a company show this span
+            company && <span> at {company}</span>
+          }
+        </p>
+        <p className="my-1">{location && <span>{location}</span>}</p>
+        <Link to={`/profile/${_id}`} className="btn btn-primary">
+          View profile
+        </Link>
+      </div>
+      <ul>
+        {
+          // get the first 5 skills
+          skills.slice(0, 4).map((skill, index) => (
+            <li key={index} className="text-primary">
+              <i className="fas fa-check" />
+              {skill}
+            </li>
+          ))
+        }
+      </ul>
+    </div>
+  );
+};
+
+ProfileItem.propTypes = {
+  profile: PropTypes.object.isRequired,
+};
+
+export default ProfileItem;
