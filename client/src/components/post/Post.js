28584a1803bd3c92f9ee7597064be1ee01123cd6
PabloMoreno2000 <pamta2000@gmail.com>
2021-02-19 09:20:23 -0600

    Comments are shown in each post discussion

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import PostItem from "../posts/PostItem";
 import CommentForm from "./CommentForm";
+import CommentItem from "./CommentItem";
 import { getPost } from "../../actions/post";
 
 // match is a default value inside props
@@ -24,6 +25,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div class="comments">
+        {post.comments.map((comment) => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
